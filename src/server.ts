88b9b96a7ac39b71a2d51dc6b6e33a11987dd2c0
Kevin Defalco <kevindefalco@gmail.com>
2023-07-19 19:19:32 -0300

    creando container en docker y bbdd con sql

--- src/server.ts
+++ src/server.ts
@@ -2,28 +2,30 @@ import express from "express"
 import morgan from "morgan"
 import cors from "cors"
 import { UserRouter } from "./router/user.router"
+import { ConfigServer } from "./config/config"
 
-class ServerBootstrap {
+class ServerBootstrap extends ConfigServer {
     public app: express.Application = express()
-    private port : number =  8000
+    private port: number = this.getNumberEnv('PORT') 
 
-    constructor(){
+    constructor() {
+        super()
         this.app.use(express.json())
-        this.app.use(express.urlencoded({extended: true}))
+        this.app.use(express.urlencoded({ extended: true }))
         this.app.use(morgan("dev"))
         this.app.use(cors())
         this.app.use('/api', this.routers())
         this.listen()
-    
+
     }
     routers(): Array<express.Router> {
         return [new UserRouter().router]
     }
 
-    public listen(){
-        this.app.listen(this.port, ()=>{
+    public listen() {
+        this.app.listen(this.port, () => {
             console.log(`Server running on port ${this.port}`);
-            
+
         })
     }
 }
